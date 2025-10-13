7579dcf3aadd4228755847d471bfec4eafc007a6
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-10-13 16:56:30 +0330

    Add files via upload

--- src/pages/main/components/home/Home.js
+++ src/pages/main/components/home/Home.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import './Home.css'
 
-function Home() {
+function Home () {
   return (
     <div className='home'>
-        <div className='homeContent'>
-            <h1 className='myName' style={{fontSize:'72px', color:'#fff'}}>
-                Hi, I'm <span style={{color:'#D8B4FE'}}>Fatemeh Madihi</span>
-            </h1>
-            <h2 className='myJob'>
-                Senior Frontend Developer & React Developer
-            </h2>
-            <p className='aboutMe'>
-                I craft elegant digital solutions that blend innovative technology with thoughtful design. Passionate about creating meaningful user experiences that drive business success.
-            </p>
-        </div>
+      <div className='homeContent'>
+        <h1 className='myName' style={{ fontSize: '72px', color: '#fff' }}>
+          Hi, I'm <span style={{ color: '#D8B4FE' }}>Fatemeh Madihi</span>
+        </h1>
+        <h2 className='myJob'>Senior Frontend Developer & React Developer</h2>
+        <p className='aboutMe'>
+          I craft elegant digital solutions that blend innovative technology
+          with thoughtful design. Passionate about creating meaningful user
+          experiences that drive business success.
+        </p>
+      </div>
 
-        <div class='btnGroup'>
-            <button class="BTN ExplorBTN"><i class="ri-folder-line mr-3"></i>Explore My Work</button>
-            <button class="BTN ConnectBTN"><i class="ri-chat-3-line mr-3"></i>Let's Connect</button>
-        </div>
+      <div class='btnGroup'>
+        <button class='BTN ExplorBTN'>
+          <i class='ri-folder-line mr-3'></i>Explore My Work
+        </button>
+        <button class='BTN ConnectBTN'>
+          <i class='ri-chat-3-line mr-3'></i>Let's Connect
+        </button>
+      </div>
     </div>
   )
 }
