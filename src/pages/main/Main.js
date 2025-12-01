3d1db4934734929d99e4e9c0bb621adf43c39d90
fatemehmadihi <fmadihi.2011@gmail.com>
2025-12-01 22:33:40 +0330

    add about me

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import './Main.css'
-import BackImg from './components/backImage/BackImg'
-import Experience from './components/experience/Experience'
+import React from "react";
+import "./Main.css";
+import BackImg from "./components/backImage/BackImg";
+import Experience from "./components/experience/Experience";
+import AboutMe from "./components/aboutMe/AboutMe";
 function Main() {
   return (
-    <div style={{width:'100%'}} className='mainC'>
-        <BackImg />
-        {/* <Experience /> */}
+    <div style={{ width: "100%" }} className="mainC">
+      <BackImg />
+      <AboutMe />
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
---
+++ src/pages/main/components/aboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutMe.css'
+
+function AboutMe(props) {
+    return (
+        <div className='AboutMe-Wrapper'>
+            
+        </div>
+    );
+}
+
+export default AboutMe;
